fd9a0d07cacc65586528890e39d9d0c7a4bb04d7
Eric Pantheon <emutema.mutema@gmail.com>
2024-12-25 13:36:49 +0300

    Tailwind changes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import BoardAnalytics from './components/BoardAnalytics';
 import './App.css';
+import './globals.css';  // This is for shadcn/ui styles
 
 function App() {
   return (
-    <div className="App">
+    <div className="App bg-gray-50 min-h-screen">
       <BoardAnalytics />
     </div>
   );
